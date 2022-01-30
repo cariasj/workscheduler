ac95f59e448b621fd6c6f695ac8937bb9738ce2b
charon <cariasjosue@gmail.com>
2022-01-30 18:32:06 -0500

    js and date. Pending time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var currentDate = moment().format('dddd, MMM Do YYYY');
 $("#day").html(currentDate);
 
+var currentTime = moment().format('hhh, mmm');
+$("#time").html(currentTime);
+
 $(document).ready(function () {
     // saveBtn click listener 
     $(".saveBtn").on("click", function () {
@@ -8,6 +11,50 @@ $(document).ready(function () {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+
+    function timeTracker() {
+        //get current number of hours.
+        var timeNow = moment().hour();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // To check the time and add the classes for background indicators
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+    // Get item from local storage if any
+    $("#nine .description").val(localStorage.getItem("nine"));
+    $("#ten .description").val(localStorage.getItem("ten"));
+    $("#eleven .description").val(localStorage.getItem("eleven"));
+    $("#twelve .description").val(localStorage.getItem("twelve"));
+    $("#one .description").val(localStorage.getItem("one"));
+    $("#two .description").val(localStorage.getItem("two"));
+    $("#three .description").val(localStorage.getItem("three"));
+    $("#four .description").val(localStorage.getItem("four"));
+    $("#five .description").val(localStorage.getItem("five"));
+
+    timeTracker();
 
 
 
@@ -16,6 +63,4 @@ $(document).ready(function () {
 
 
 
-
-})
 })
