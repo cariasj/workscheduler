f7c5b3cbb91b66e52361500ca020eb0d1fcf7799
charon <cariasjosue@gmail.com>
2022-01-30 17:51:50 -0500

    js function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,21 @@
 var currentDate = moment().format('dddd, MMM Do YYYY');
 $("#day").html(currentDate);
+
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+
+
+
+
+
+
+
+
+
+})
+})
